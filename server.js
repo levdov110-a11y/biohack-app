7d9082c626865495a77f848d691db3d7ad545ba4
levdov110-a11y <levdov110@gmail.com>
2026-02-24 14:14:18 +0200

    Refactor server to use Express and Axios

--- server.js
+++ server.js
@@ -1,14 +1,36 @@
-const cors_proxy = require('cors-anywhere');
-const http = require('http');
+const express = require('express');
+const axios = require('axios');
+const cheerio = require('cheerio');
+const app = express();
 
-cors_proxy.createServer({
-    requireHeader: ['x-my-token'],
-    handleInitialRequest: (req, res, location) => {
-        if (req.headers['x-my-token'] !== '23603892') { 
-            res.writeHead(403);
-            res.end('No Access');
-            return true;
-        }
-        return false;
+const PASSWORD = "23603892";
+
+app.get('/', async (req, res) => {
+    const targetUrl = req.query.url;
+    const userToken = req.query.token;
+
+    if (userToken !== PASSWORD) return res.send("Access Denied");
+    if (!targetUrl) return res.send("No URL provided");
+
+    try {
+        const response = await axios.get(targetUrl, {
+            headers: { 'User-Agent': 'Mozilla/5.0' }
+        });
+
+        const $ = cheerio.load(response.data);
+
+        // קסם: משנה את כל הקישורים באתר כדי שיעברו דרך השרת שלך שוב
+        $('a').each((i, el) => {
+            const href = $(el).attr('href');
+            if (href && href.startsWith('http')) {
+                $(el).attr('href', `?token=${PASSWORD}&url=${encodeURIComponent(href)}`);
+            }
+        });
+
+        res.send($.html());
+    } catch (e) {
+        res.send("Error loading site: " + e.message);
     }
-}).listen(process.env.PORT || 8080, '0.0.0.0');
+});
+
+app.listen(process.env.PORT || 8080);
