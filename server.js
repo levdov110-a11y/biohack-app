6d919dd8f51e8396686838d335305abe30b56362
levdov110-a11y <levdov110@gmail.com>
2026-02-24 07:23:05 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,14 @@
 const cors_proxy = require('cors-anywhere');
 const http = require('http');
 
-const host = '0.0.0.0';
-const port = process.env.PORT || 8080;
-
-// השרת הזה יסכים להעביר מידע רק אם ה-Token נכון
 cors_proxy.createServer({
-    originWhitelist: [], // מאפשר לכולם, אבל נחסום עם ה-Token
     requireHeader: ['x-my-token'],
     handleInitialRequest: (req, res, location) => {
-        if (req.headers['x-my-token'] !== '12345-secret') { // שים פה את הסיסמה שלך
+        if (req.headers['x-my-token'] !== '23603892') { 
             res.writeHead(403);
-            res.end('Access Denied');
+            res.end('No Access');
             return true;
         }
         return false;
     }
-}).listen(port, host, () => {
-    console.log('Running on ' + host + ':' + port);
-});
+}).listen(process.env.PORT || 8080, '0.0.0.0');
